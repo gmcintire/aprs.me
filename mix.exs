36aad374e943c450204bf32e8718ca32e5707cca
Graham McIntire <gmcintire@corvusinsurance.com>
2022-04-11 13:59:01 -0500

    add os_mon

--- mix.exs
+++ mix.exs
@@ -21,7 +21,7 @@ defmodule Aprs.MixProject do
   def application do
     [
       mod: {Aprs.Application, []},
-      extra_applications: [:logger, :runtime_tools]
+      extra_applications: [:logger, :runtime_tools, :os_mon]
     ]
   end
 
